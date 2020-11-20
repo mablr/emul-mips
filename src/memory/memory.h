6fb73e6e9ad71218a8468cdb45896f18a23822d6
mablr <59505383+mablr@users.noreply.github.com>
2020-11-20 22:14:45 +0100

    Updating headers and adding some comments

--- src/memory/memory.h
+++ src/memory/memory.h
@@ -10,11 +10,24 @@ struct byteMem{
 };
 typedef byteMem * memory;
 
-void insertWord(unsigned int address, unsigned int data, liste * memory);
+/* Ajoute un octet de données dans la mémoire à l'adresse indiquée */
 void insertByteMem(unsigned int address, unsigned char data, liste * memory);
-unsigned int getWord(unsigned int address, liste * memory);
+
+/* Ajoute un mot de données (4 octets) dans la mémoire à l'adresse indiquée */
+void insertWord(unsigned int address, unsigned int data, liste * memory);
+
+/* Récupère en mémoire un octet de données à l'adresse indiquée */
 unsigned char getByteMem(unsigned int address, liste * memory);
-void deleteWord(unsigned int address, liste * memory);
+
+/* Récupère en mémoire un mot de données à l'adresse indiquée */
+unsigned int getWord(unsigned int address, liste * memory);
+
+/* Libère l'emplacement mémoire à l'adresse indiquée */
 void deleteByteMem(unsigned int address, liste * memory);
+
+/* Vérifie si un emplacement mémoire est alloué */
+unsigned int isAllocatedByteMem(unsigned int address, liste * memory);
+
+/* Affiche une représentation de la mémoire */
 void showMemory(liste * memory);
 #endif

3a6e4a748e30f3afac7c63d514a8e600d89507ab
mablr <59505383+mablr@users.noreply.github.com>
2020-11-20 22:19:30 +0100

    Adding some comments

--- src/register/register.c
+++ src/register/register.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "register.h"
 
+/* Tableau d'entiers signés pour stocker les valeurs de registres */
 int registers[NB_REGISTERS];
 
+/* Stocke la valeur donnée si le registre indiqué existe */
 void storeRegister(int index, int value){
     if(index >= 0 && index < NB_REGISTERS){
         registers[index] = value;
     }
 }
+
+/* Retourne valeur dans le registre indiqué s'il existe */
 int getRegister(int index){
     int value = 0;
     if(index >= 0 && index < NB_REGISTERS){

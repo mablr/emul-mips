cf0b2318fe831d27867f299df8af373afdeb4998
mablr <59505383+mablr@users.noreply.github.com>
2020-12-01 16:58:28 +0100

    Adding showRegisters function

--- src/register/register.c
+++ src/register/register.c
@@ -19,3 +19,11 @@ int getRegister(int index){
     }
     return value;
 }
+/* Affiche les registres */
+void showRegisters(void){
+    int i;
+    for(i = 0; i < GENERAL_REGISTERS; i++){
+        printf("$%d : %d\n", i, registers[i]);
+    }
+    printf("HI : %d \t LO : %d \t PC : %d\n", registers[33], registers[34], registers[32]);
+}

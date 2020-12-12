26bbb7c02656e70f7f14678c58a6886ba57e5e30
mablr <59505383+mablr@users.noreply.github.com>
2020-12-12 20:46:46 +0100

    Refactoring instruction functions

--- src/function/function.c
+++ src/function/function.c
@@ -1,129 +1,101 @@
 #include <stdio.h>
 #include "../register/register.h"
+#include "../memory/memory.h"
 
-/*Pas faites :  Store Word , Load Word, Load Upper Immediate, Jump Register, Jump and Link, No Operation*/
+/*Pas faites :  Store Word , Load Word */
 
 /* ADD WORD */
 int add(int args[]){
-    int overflowStatus=0;
-    long fullResult = getRegister(args[1]) + getRegister(args[2]);
-    int value;
-    /* Si débordement (overflow) */
-    if(fullResult & 0xFFFFFFFF00000000){
-        overflowStatus = 1;
-    }else{
-        value = fullResult & 0xFFFFFFFF;
-        storeRegister(args[0], value);
-    }
-    return overflowStatus;
+    storeRegister(args[0], getRegister(args[1]) + getRegister(args[2]));
+    return 0;
 }
 /* Add Immediate Word */
 int addi(int args[]){
-    int overflowStatus=0;
-    int value = getRegister(args[1]) + args[2];
-    if(value +1 == 0)
-        overflowStatus = 1;
-    if(!overflowStatus)
-        storeRegister(args[0], value);
-    return overflowStatus;
+    storeRegister(args[0], getRegister(args[1]) + args[2]);
+    return 0;
 }
 /* AND */
 int and_(int args[]){
-    int value = getRegister(args[1]) & getRegister(args[2]);
-    storeRegister(args[0],value);
+    storeRegister(args[0], getRegister(args[1]) & getRegister(args[2]));
     return 0;
 }
 /* Branch on Equal */
 int beq(int args[]){
-    if(getRegister(args[0]) == getRegister(args[1]))
-        registers[32] += getRegister(args[2]);
+    if(getRegister(args[0]) == getRegister(args[1])){
+        storeRegister(PC, getRegister(PC) + args[2]);
+    }
     return 0;
 } 
 /* Branch on Greater Than Zero */
 int bgtz(int args[]){
-    if(getRegister(args[0]) > 0)
-        registers[32] += getRegister(args[1]);
+    if(getRegister(args[0]) > 0){
+        storeRegister(PC, getRegister(PC) + args[1]);
+    }
     return 0;
 } 
 /* Branch on Less Than or Equal to Zero */
 int blez(int args[]){
-    if(getRegister(args[0]) <= 0)
-        registers[32] += getRegister(args[1]);
+    if(getRegister(args[0]) <= 0){
+        storeRegister(PC, getRegister(PC) + args[1]);
+    }
     return 0;
 } 
 /* Branch on Not Equal */
 int bne(int args[]){
-    int condition = getRegister(args[1]) != getRegister(args[2]);
-    if(condition)
-        registers[32] += getRegister(args[2]);
+    if(getRegister(args[1]) != getRegister(args[2])){
+        storeRegister(PC, getRegister(PC) + args[2]);
+    }
     return 0;
 } 
 /* Divide Word */
 int div_(int args[]){
-    int reste, quotient;
-    reste=getRegister(args[1])% getRegister(args[2]);
-    quotient= getRegister(args[1]) / getRegister(args[2]);
-    storeRegister(LO, quotient); /*quotient dans le registre LO*/
-    storeRegister(HI, reste);    /*reste dans le registre HI*/
+    /* quotient dans le registre LO */
+    storeRegister(LO, getRegister(args[1]) / getRegister(args[2]));
+    /* reste dans le registre HI */
+    storeRegister(HI, getRegister(args[1]) % getRegister(args[2]));
     return 0;
 }
-/* Jump */
+/* Jump */ /* Vérifier la valeur du PC en sortie de boucle après un Jump */
 int j(int args[]){
-    storeRegister(32, getRegister(args[0])); 
+    /*PC+1  = GPRLEN-1..28 || instr_index || 0^2*/
+    storeRegister(PC, (getRegister(PC) & 0xF0000000) | (args[0]<<2 & 0x0FFFFFFC));
     return 0;
 }
-/* Or */
-int or_(int args[]){
-    int value = getRegister(args[1]) | getRegister(args[2]);
-    storeRegister(args[0],value);
+/* Jump and Link */
+int jal(int args[]){
+    /* Return Adress */
+    storeRegister(31, getRegister(PC) + 8);
+    /* Comme un JUMP normal */
+    storeRegister(PC, (getRegister(PC) & 0xF0000000) | (args[0]<<2 & 0x0FFFFFFC));
     return 0;
 }
-/* Rotate Word Right */
-int rotr(int args[]){
-    int errorFlag=0;
-    int value = getRegister(args[1]) >> getRegister(args[2]);
-    storeRegister(args[0], value);
-    return errorFlag;
-}
-/* Shift Word Left Logical */
-int sll(int args[]){
-    int value = getRegister(args[1]) << getRegister(args[2]);
-    storeRegister(args[0], value);
+/* Jump Register */
+int jr(int args[]){
+    /* Alignement de force de l'adresse contenue dans le registre en argument,
+     * sans exception comme dans MIPS I, le champ HINT étant ignoré (tjr à 0).
+     */
+    storeRegister(PC, getRegister(args[0]) & 0x0FFFFFFC);
     return 0;
 }
-
-/* Shift Word Right Logical */
-int srl(int args[]){
-    int value = getRegister(args[1]) >> getRegister(args[2]);
-    storeRegister(args[0], value);
+/* Load Upper Immediate */
+int lui(int args[]){
+    storeRegister(args[0], getRegister(args[1])<<16);
     return 0;
 }
-
-/* Set on Less Than */ 
-int slt(int args[]){
-    int status=0;
-    if(getRegister(args[1]) < getRegister(args[2]))
-        status=1;
-    return status;
-}
-/*Subtract Word*/
-int sub(int args[]){
-    int overflowStatus = 0;
-    int value = getRegister(args[1]) - getRegister(args[2]);
-    storeRegister(args[0], value);
-    return overflowStatus;
-}
-/* Exclusive OR*/
-int xor_(int args[]){
-    int value = getRegister(args[1]) ^ getRegister(args[2]);
-    storeRegister(args[0],value);
+/*********************************/
+/*Load Word*/
+int lw(int args[]){
+    unsigned int address = args[1] + getRegister(args[2]);
+    /* Alignée de force sur un mot */
+    address -= address%4;
+    /* Chargement de la valeur en mémoire vers le registre */
+    storeRegister(args[0], getWord(address, &progMem));
     return 0;
 }
-/*Multiply Word*/
-int mult(int args[]){
-    long value = getRegister(args[1])*getRegister(args[2]);
-    registers[HI]=(value & 0xFFFFFFFF00000000) >> 32;  /* Bits de poids forts dans HI*/
-    registers[LO]=(value & 0x00000000FFFFFFFF);        /* Bits de poids faibles dans LO*/
+/*********************************/
+/*Move From HI Register*/
+int mfhi(int args[]){
+    storeRegister(args[0], getRegister(args[HI]));
     return 0;
 }
 /*Move From LO Register */
@@ -131,32 +103,63 @@ int mflo(int args[]){
     storeRegister(args[0], getRegister(args[LO]));
     return 0;
 }
-/*Move From HI Register*/
-int mfhi(int args[]){
-    storeRegister(args[0], getRegister(args[HI]));
+/*Multiply Word*/
+int mult(int args[]){
+    long int value = (long int)getRegister(args[1]) * (long int)getRegister(args[2]);
+    /* Bits de poids forts dans HI*/
+    storeRegister(HI, (value & 0xFFFFFFFF00000000) >> 32);
+    /* Bits de poids faibles dans LO*/
+    storeRegister(LO, value & 0x00000000FFFFFFFF);
     return 0;
 }
 /*No Operation*/
 int nop(int args[]){
     return 0;
 }
-/*Load Word*/
-int lw(int args[]){
+/* Or */
+int or_(int args[]){
+    storeRegister(args[0], getRegister(args[1]) | getRegister(args[2]));
+    return 0;
+}
+/* Rotate Word Right */
+int rotr(int args[]){
+    /* value = GPR[rt] s-1..0 || GPR[rt] 31..s */
+    int value = (getRegister(args[1]) << (31 - getRegister(args[2]))) | (getRegister(args[1]) >> getRegister(args[2]));
+    storeRegister(args[0], value);
+    return 0;
+}
+/* Shift Word Left Logical */
+int sll(int args[]){
+    storeRegister(args[0], getRegister(args[1]) << getRegister(args[2]));
+    return 0;
+}
+/* Set on Less Than */ 
+int slt(int args[]){
+    int value = getRegister(args[1]) < getRegister(args[2]) ? 1 : 0;
+    storeRegister(args[0], value);
+    return 0;
+}
+/* Shift Word Right Logical */
+int srl(int args[]){
+    storeRegister(args[0], getRegister(args[1]) >> getRegister(args[2]));
+    return 0;
+}
+/*Subtract Word*/
+int sub(int args[]){
+    storeRegister(args[0], getRegister(args[1]) - getRegister(args[2]));
     return 0;
 }
 /*Store Word*/
 int sw(int args[]){
+    unsigned int address = args[1] + getRegister(args[2]);
+    /* Alignée de force sur un mot */
+    address -= address%4;
+    /* Stockage de la valeur du registre rt en mémoire */
+    insertWord(address, getRegister(args[0]), &progMem);
     return 0;
 }
-/* Load Upper Immediate */
-int lui(int args[]){
-    return 0;
-}
-/* Jump Register */
-int jr(int args[]){
-    return 0;
-}
-/* Jump and Link */
-int jal(int args[]){
+/* Exclusive OR*/
+int xor_(int args[]){
+    storeRegister(args[0], getRegister(args[1]) ^ getRegister(args[2]));
     return 0;
 }
